fc3360fcc0537761ea1fb9ffd5a6b38362422ee6
apex361co-eng <apex361.co@gmail.com>
2026-07-24 17:23:34 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='maalinex-v254';
+const C='maalinex-v255';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim();});
@@ -6,11 +6,7 @@ self.addEventListener('fetch',e=>{
   const u=new URL(e.request.url);
   if(e.request.method!=='GET')return;
   if(u.origin===location.origin){
-    e.respondWith(
-      fetch(e.request).then(res=>{
-        const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl));return res;
-      }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
-    );
+    e.respondWith(fetch(e.request).then(res=>{const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl));return res;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
   } else if(u.hostname.includes('jsdelivr')){
     e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl));return res;})));
   }
