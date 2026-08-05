c42e84f5096d4bb4999881daeab7672ee12e58ab
apex361co-eng <apex361.co@gmail.com>
2026-08-05 23:05:57 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='maalinex-v376';
+const C='maalinex-v377';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl));return res}).catch(()=>caches.match('./index.html'))))});
